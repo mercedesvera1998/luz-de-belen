0f5581fa349ce6ac409ffbcc3d023c8533e4587a
mercedesvera1998 <108768147+mercedesvera1998@users.noreply.github.com>
2022-08-01 14:36:25 -0300

    Add files via upload

---
+++ luz de belen/javascript.js
@@ -0,0 +1,14 @@
+function alerta(){
+alerta = prompt ("¿Estas Seguro/a que quieres cancelar?"); 
+if (alerta === 'si'){
+    alert("Registro cancelado.");
+}
+else{ 
+    alert("Continua con tu registro."); 
+}
+}
+
+function felicitaciones(){
+    alert("¡Muchas gracias por ser parte de Luz de belén! Tenes un 10% de descuento en toda la tienda."); 
+}
+  
